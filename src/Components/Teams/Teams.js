271fa001166588f6be322840e9bdd20ee2a27e47
Sadovei Radu <radu.sadovei@gmail.com>
2022-03-29 13:03:34 +0300

    wip

--- src/Components/Teams/Teams.js
+++ src/Components/Teams/Teams.js
@@ -7,11 +7,17 @@ export default function Teams({ data }) {
         <>
             <div className="left-team-wrapper">
                 <div className="logo" style={{ backgroundImage: `url(http://redis-birou.pgl.ro/pgl/resources/csgo/team/${data.leftSide.nameKey}/logo.webp)` }}></div>
-                <p className="team-name font-tablet">{data.leftSide.name.toUpperCase()}</p>
+
+                <div className='name-wrapper'>
+                    <p className="team-name">{data.leftSide.name.toUpperCase()}</p>
+                </div>
             </div>
 
             <div className="right-team-wrapper">
-                <p className="team-name font-tablet">{data.rightSide.name.toUpperCase()}</p>
+                <div className='name-wrapper'>
+                    <p className="team-name">{data.rightSide.name.toUpperCase()}</p>
+                </div>
+
                 <div className="logo" style={{ backgroundImage: `url(http://redis-birou.pgl.ro/pgl/resources/csgo/team/${data.rightSide.nameKey}/logo.webp)` }}></div>
             </div>
         </>
