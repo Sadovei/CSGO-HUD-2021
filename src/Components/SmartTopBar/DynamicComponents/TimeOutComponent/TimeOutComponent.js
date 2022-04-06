3ab5dea10f694ad16cf83bc33803e154508f1c79
Sadovei Radu <radu.sadovei@gmail.com>
2022-04-06 13:02:15 +0300

    final changes + need video major

--- src/Components/SmartTopBar/DynamicComponents/TimeOutComponent/TimeOutComponent.js
+++ src/Components/SmartTopBar/DynamicComponents/TimeOutComponent/TimeOutComponent.js
@@ -18,10 +18,18 @@ export default function TimeOutComponent({ topBarData }) {
         top: topBarData.round.phase === 'timeout_t' || topBarData.round.phase === 'timeout_ct' || topBarData.round.phase === 'paused'
             ? '15%' : '14%'
     })
+
+    let teamKeyLogo = timeOut === 'CT' ?
+        (topBarData.leftSide.sideTeam === 'CT' ? topBarData.leftSide.nameKey : topBarData.rightSide.nameKey) : timeOut === 'T' &&
+        (topBarData.leftSide.sideTeam === 'T' ? topBarData.leftSide.nameKey : topBarData.rightSide.nameKey)
+
+    let logo = teamKeyLogo === 'placeholder' ? `placeholder/${topBarData.leftSide.sideTeam === timeOut ? topBarData.leftSide.sideTeam : topBarData.rightSide.sideTeam === timeOut && topBarData.rightSide.sideTeam}` :
+        teamKeyLogo
+
     return (
         <animated.div className='timeOut-wrapper row' style={{ opacity: mvpProps.opacity, top: mvpProps.top }}>
             <div className='logo-wrapper'>
-                <div className={`logo ${timeOut}`}></div>
+                <div className={`logo ${timeOut}`} style={{ backgroundImage: `url(http://redis-birou.pgl.ro/pgl/resources/csgo/team/${logo}/logo.webp)` }}></div>
             </div>
 
             <div className={`content-wrapper col ${timeOut}`}>
@@ -41,7 +49,10 @@ export default function TimeOutComponent({ topBarData }) {
                 </div>
 
                 <p className={`textSide ${timeOut}`}>
-                    {timeOut === 'CT' ? `COUNTER TERRORIST` : timeOut === 'T' && `TERRORIST`}{' '}
+                    {timeOut === 'CT' ?
+                        (topBarData.leftSide.sideTeam === 'CT' ? topBarData.leftSide.name : topBarData.rightSide.name) :
+                        timeOut === 'T' &&
+                        (topBarData.leftSide.sideTeam === 'T' ? topBarData.leftSide.name : topBarData.rightSide.name)}
                 </p>
             </div>
         </animated.div>
