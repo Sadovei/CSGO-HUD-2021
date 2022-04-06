989f4ab946f63b27f64e302d138a1396352ceb08
Sadovei Radu <radu.sadovei@gmail.com>
2022-04-06 14:39:30 +0300

    fix

--- src/utils/socketIO.js
+++ src/utils/socketIO.js
@@ -1,16 +1,16 @@
 import { findGetParameter } from "./URLParameters";
 import socketClient from "socket.io-client";
 
-const { REACT_APP_BACKEND, REACT_APP_PORT } = process.env
+const { REACT_APP_STREAM } = process.env
 
 export const token = findGetParameter("token") || 'main';
-const backEND = REACT_APP_BACKEND
-const port = REACT_APP_PORT
+const backEND = '10.97.2.31'
+const port = REACT_APP_STREAM === 'A' ? 4400 : 4600
 const ENDPOINT = `http://${backEND}:${port}/?client=igdir`;
 const socket = socketClient(ENDPOINT);
 
-const backENDPARSER = '10.97.2.240'
-const PORTPARSER = 4400
+const backENDPARSER = '10.97.2.31'
+const PORTPARSER = REACT_APP_STREAM === 'A' ? 4500 : 4700
 const ENDPOINTPARSER = `http://${backENDPARSER}:${PORTPARSER}/?client=${token}`
 const socketParser = socketClient(ENDPOINTPARSER)
 
@@ -27,12 +27,6 @@ export const subscribeToTopBar = (cb) => {
 }
 
 export const subscribeToParser = (cb) => {
-  let prevParserData = ''
-  socketParser.on(`ParserConnection`, (data) => {
-    if (prevParserData !== JSON.stringify(data)) {
-      prevParserData = JSON.stringify(data)
-      cb(data)
-    }
-  })
+  socketParser.on(`ParserConnection`, (data) => cb(data))
   socketParser.emit(`subscribe`, `ParserConnection`)
 }
