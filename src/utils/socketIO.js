29de498a9ba4dc39d52dfebaa50b34d9074a5aa5
Sadovei Radu <radu.sadovei@gmail.com>
2022-04-11 13:38:42 +0300

    add token problem

--- src/utils/socketIO.js
+++ src/utils/socketIO.js
@@ -3,11 +3,11 @@ import socketClient from "socket.io-client";
 
 const { REACT_APP_STREAM } = process.env
 
-export const token = findGetParameter("token") || 'main';
+export const token = findGetParameter("client") || 'main';
 const backEND = '10.97.2.31'
 // const backEND = '10.97.2.14'
 const port = REACT_APP_STREAM === 'A' ? 4400 : 4600
-const ENDPOINT = `http://${backEND}:${port}/?client=igdir`;
+const ENDPOINT = `http://${backEND}:${port}/?client=${token}`;
 const socket = socketClient(ENDPOINT);
 
 const PORTPARSER = REACT_APP_STREAM === 'A' ? 4500 : 4700
@@ -17,13 +17,13 @@ const socketParser = socketClient(ENDPOINTPARSER)
 export const subscribeToTopBar = (cb) => {
   let prevTopBarData = ''
 
-  socket.on(`igdir_OverlayTopBar`, (data) => {
+  socket.on(`${token}_OverlayTopBar`, (data) => {
     if (prevTopBarData !== JSON.stringify(data)) {
       prevTopBarData = JSON.stringify(data)
       cb(data)
     }
   })
-  socket.emit(`subscribe`, `igdir_OverlayTopBar`);
+  socket.emit(`subscribe`, `${token}_OverlayTopBar`);
 }
 
 export const subscribeToParser = (cb) => {
