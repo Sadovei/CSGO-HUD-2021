70682c3f1eeaf81858f1dc4e0cf2b13be31d1c31
Sadovei Radu <radu.sadovei@gmail.com>
2022-04-12 18:18:22 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { subscribeToParser, subscribeToTopBar } from "./utils/socketIO";
+
 import Comercials from "./Components/Comercials/Comercials";
-import React from "react";
 import SmartDynamic from "./Components/SmartDynamic/SmartDynamic";
 
 function App() {
+  const [topBar, setTopBar] = useState({ round: { phase: "" } });
+  const [sponsor, setSponsor] = useState({ data: {}, type: '', show: false });
+
+  useEffect(() => {
+    subscribeToTopBar(data => {
+      setTopBar(data)
+    })
+
+    subscribeToParser(data => {
+      setSponsor(data)
+    })
+  }, [])
+
   return (
     <>
-      <Comercials />
-      <SmartDynamic />
+      {topBar.round.phase && <Comercials phase={topBar.round.phase} />}
+      <SmartDynamic data={sponsor} />
     </>
   );
-
 }
 
 export default App;
--- src/Components/Comercials/Comercials.js
+++ src/Components/Comercials/Comercials.js
@@ -1,69 +1,37 @@
 import './Comercials.scss'
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { subscribeToTopBar } from '../../utils/socketIO'
+import ImagesComponent from './ImagesComponent/ImagesComponent'
 
-export default function Comercials() {
+export default function Comercials({ phase }) {
     const [animClass, setAnimClass] = useState('')
-    const [topBar, setTopBar] = useState({ round: { phase: '' } });
-    const counter = useRef(0)
-    const sponsorsIMG = useRef([])
-    const SVGMap = useRef({})
 
     useEffect(() => {
-        subscribeToTopBar(data => {
-            setTopBar(data)
-        })
-
-        sponsorsIMG.current = require.context('./icons', true).keys().map((key) =>
-            key.substring(key.lastIndexOf('/') + 1, key.lastIndexOf('.'))
-        )
-
-        SVGMap.current = require.context('./icons', true).keys().reduce((images, path) => {
-            const key = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.'))
-            images[key] = require.context('./icons', true)(path).default
-            return images
-        }, {})
-    }, [])
-
-    useEffect(() => {
-        if (topBar.round.phase === 'freezetime' || topBar.round.phase === 'timeout_t' || topBar.round.phase === 'timeout_ct') {
+        if (phase === 'freezetime' || phase === 'timeout_t' || phase === 'timeout_ct') {
             setAnimClass('showStart')
         }
 
-        if (topBar.round.phase === 'live') {
+        if (phase === 'live') {
             setTimeout(() => {
                 setAnimClass('hideStart')
             }, 5000)
         }
 
-        if (topBar.round.phase === 'bomb') {
+        if (phase === 'bomb') {
             setAnimClass('showBomb')
         }
 
-        if (topBar.round.phase === 'bomb')
+        if (phase === 'bomb')
             setTimeout(() => {
                 setAnimClass('hideBomb')
             }, 5000)
-    }, [topBar.round.phase])
-    
-    useEffect(() => {
-        if (counter.current === sponsorsIMG.length)
-            counter.current = 0
-        else
-            setInterval(() => {
-                counter.current++
-            }, 15000)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [counter.current])
-    
+        return null
+    }, [phase])
+
     return (
         <div className={`content-wrapper-comercial ${animClass}`}>
-            <div
-                className='sponsor-image'
-                style={{ backgroundImage: `url(${SVGMap.current[sponsorsIMG.current[counter.current]]})` }}
-            ></div>
+            <ImagesComponent />
         </div>
     )
 }
