debcd4fa47b81dad982c2d887c6f5219aec7bdec
Sadovei Radu <radu.sadovei@gmail.com>
2022-04-13 19:43:30 +0300

    fix

--- src/utils/socketIO.js
+++ src/utils/socketIO.js
@@ -4,7 +4,7 @@ import socketClient from 'socket.io-client'
 const { REACT_APP_STREAM } = process.env
 
 export const token = findGetParameter('client') || 'main'
-const backEND = '10.97.2.31' 
+const backEND = '10.97.2.31'
 // const backEND = '10.97.2.14'
 const port = REACT_APP_STREAM === 'A' ? 4400 : 4600
 const ENDPOINT = `http://${backEND}:${port}/?client=${token}`
